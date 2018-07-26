b3bbcdc5dd3697dcaeabc02f8b2d237cd3b8a450
unknown <181710131@LAB30809.senacinformatica.br>
2018-07-26 16:20:54 -0300

    Criação do arquivo Produto

--- src/classes/Produto.java
+++ src/classes/Produto.java
@@ -1,10 +1,16 @@
 
 package classes;
 
+import java.sql.Date;
+
 public class Produto {
     private int codigo;
     private String nome;
-    private String email;
+    private int quantidade;
+    private double preco;
+    private Date fabricacao;
+    private Date validade;
+    
 
     public Produto() {
     }
@@ -12,16 +18,16 @@ public class Produto {
     public Produto(int codigo, String nome , String email) {
         this.codigo = codigo;
         this.nome = nome;
-        this.email = email;
+        this.quantidade = quantidade;
+        this.preco = preco;
+        this.fabricacao = fabricacao;
+        this.validade = validade;
     }
-
+    
     @Override
     public String toString() {
         return nome;
-    }
-
-    
-    
+    }  
     
     public int getCodigo() {
         return codigo;
@@ -38,13 +44,41 @@ public class Produto {
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
-    public String getEmail() {
-        return email;
+
+    public int getQuantidade() {
+        return quantidade;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Date getFabricacao() {
+        return fabricacao;
+    }
+
+    public void setFabricacao(Date fabricacao) {
+        this.fabricacao = fabricacao;
+    }
+
+    public Date getValidade() {
+        return validade;
+    }
+
+    public void setValidade(Date validade) {
+        this.validade = validade;
+    }
+        
+
+    
+   
     
 }
