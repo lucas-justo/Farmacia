23000d0677d5ecc934f939856a0eed013a5bb6b1
Luan <luan967@hotmail.com>
2018-08-01 16:36:07 -0300

    Alteração no ListProduto

--- src/telas/ListProduto.java
+++ src/telas/ListProduto.java
@@ -62,13 +62,17 @@ public class ListProduto extends javax.swing.JInternalFrame {
         tableProdutos.setForeground(new java.awt.Color(204, 204, 204));
         tableProdutos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {},
-                {},
-                {},
-                {}
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null}
             },
             new String [] {
-
+                "Título 1", "Título 2", "Título 3", "Título 4", "Título 5", "Título 6", "Título 7", "Título 8"
             }
         ));
         jScrollPane1.setViewportView(tableProdutos);
